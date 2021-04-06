4096b5b427ac44aefb1ad93c0675bdc05ca574fc
rez120 <r.pishkar121@gmail.com>
2021-04-07 01:12:15 +0430

    added country and description to card-template

--- index.php
+++ index.php
@@ -33,11 +33,12 @@ while($c<count($destinations) && $n<2){ ?>
     <div class="card" style="width: 18rem;">
   <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Agrigent_BW_2012-10-07_13-09-13.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title"><?php echo htmlspecialchars($destinations[$c]["name"]); ?></h5>
+    <h5 class="card-title"><?php echo htmlspecialchars($destinations[$c]["name"]. ", ". $destinations[$c]["country"]); ?></h5>
     <h5 class="card-title"><?php echo htmlspecialchars($destinations[$c]["company"]); ?></h5>
     <h5 class="card-title"><?php echo htmlspecialchars($destinations[$c]["flight_price"]); ?></h5>
     <h5 class="card-title"><?php echo htmlspecialchars($destinations[$c]["hotel_price"]); ?></h5>
     <p class="card-text"><?php echo htmlspecialchars($destinations[$c]["continent"]); ?></p>
+    <p class="card-text"><?php echo htmlspecialchars($destinations[$c]["description"]); ?></p>
     <a href="#" class="btn btn-primary card--btn">Go somewhere</a>
   </div>
 </div>
